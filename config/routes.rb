eb8f15639fc80031cc39798eb95aa47af7cf62a7
Xuan Ky Le <xl237@cornell.edu>
2014-04-09 14:55:08 -0400

    Change the devise views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Pinteresting::Application.routes.draw do
   devise_for :users
   root 'pages#home'
   get 'about' => 'pages#about'     #creates about_path
+  
 
   
   # The priority is based upon order of creation: first created -> highest priority.
