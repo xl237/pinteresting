1c99acfbb5d936ed3db72b87260bccb35733d918
Xuan Ky Le <xl237@cornell.edu>
2014-04-11 14:33:13 -0400

    Load pins in index action by reverse chronological order

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Pinteresting::Application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
   
 end
